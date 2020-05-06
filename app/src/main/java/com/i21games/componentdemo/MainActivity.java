0c155685de3b9cc890a82a18cbfa958ffe1aa35c
jinruifan <jinruifan@37hema.com>
2020-05-06 18:11:49 +0800

    修改router

--- app/src/main/java/com/i21games/componentdemo/MainActivity.java
+++ app/src/main/java/com/i21games/componentdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.i21games.componentdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.mzule.activityrouter.router.Routers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Routers.open(MainActivity.this,"demo://authentication");
+            }
+        });
     }
+
+
 }
--- module_news/src/main/java/com/i21games/module_news/AuthenticationActivity.java
+++ module_news/src/main/java/com/i21games/module_news/AuthenticationActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mzule.activityrouter.annotation.Router;
+
+@Router("authentication")
 public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
